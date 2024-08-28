90ea5d1c8eda757b51d3e2c68917d4b929dfdbc9
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-28 10:37:14 -0700

    frontend

--- frontend.sh
+++ frontend.sh
@@ -55,7 +55,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip  &>>$LOG_FILE
 VALIDATE $? "exstract frontend code"
 
-cp /home/ec2-user/expense-shell/frontend.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
 
 systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "restrted nginx"
